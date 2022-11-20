caa944ccd20fbbf90005ee7057fddafcf84ab363
214211096 <chani11275@gmail.com>
2022-11-20 12:44:02 +0200

    first commit

---
+++ MyProject.Context/MyDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using MyProject.Repositories.Entities;
+namespace MyProject.Context
+{
+    public class MyDBContext:DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = MyProjectDB; Trusted_Connection = True");
+        }
+
+
+        
+    }
+}
